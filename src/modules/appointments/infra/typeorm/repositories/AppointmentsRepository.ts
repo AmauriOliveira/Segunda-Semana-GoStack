74a859f0377741f83267518cc868f405132c7bb7
Amauri Antonio de Oliveira <amauriibate32@hotmail.com>
2020-11-29 21:59:55 -0300

    Inicio do 'Liskov Substitution Principle'

--- src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -1,18 +1,21 @@
 import { EntityRepository, Repository } from 'typeorm';
 
+import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
 
 @EntityRepository(Appointment)
-class AppointmentsRepository extends Repository<Appointment> {
+class AppointmentsRepository
+  extends Repository<Appointment>
+  implements IAppointmentsRepository {
   /**
    * findByDate: recebe uma data e pesquisa se tem algum appointment, caso sim retorna ele, caso não retorna null
    */
-  public async findByDate(date: Date): Promise<Appointment | null> {
+  public async findByDate(date: Date): Promise<Appointment | undefined> {
     const findAppointment = await this.findOne({
       where: { date }, // equivale a data(coluna da tabela):date(parametro)
     });
 
-    return findAppointment || null;
+    return findAppointment;
   }
 }
 
